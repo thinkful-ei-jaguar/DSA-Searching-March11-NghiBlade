a73451f16e92eb40d7b880ba51f28ffe8273876d
Nghi Tran <nghi.t.tran8@gmail.com>
2020-03-11 11:09:19 -0700

    updates

--- search.js
+++ search.js
@@ -8,3 +8,40 @@
  * Middles: 11, 15, 17
  * 3 tries and can't find 16
  */
+
+/**
+ * Interview question:
+ * Given a postorder traversal of a BST 8,12,10,16,25,20,15
+ * construct the BST
+ * Don't worry about runtime or other optimization for now
+ *
+ * input: [8,12,10,16,25,20,15]
+ * output:  15
+ *         /  \
+ *        10  20
+ *       /  \ / \
+ *      8  12 16 25
+ *
+ * last item in the array is root
+ * traverse from the end-1
+ * compare each item to the root
+ * add right children and then left
+ */
+
+function postOrderToBST(arr, start = 0, end = arr.length - 1) {
+  // base case
+  if (start > end) {
+    return;
+  }
+
+  let bst = new BST(arr[end]);
+  let i = 0;
+  for (i = end; i >= start; i--) {
+    if (arr[i] < bst.key) {
+      break;
+    }
+  }
+  bst.left = postOrderToBST(arr, start, i);
+  bst.right = postOrderToBST(arr, i + 1, end);
+  return bst;
+}
