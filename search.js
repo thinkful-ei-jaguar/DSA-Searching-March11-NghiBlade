4eda33ae8c9ce908e7beee3b78cc06b30949acd9
Nghi Tran <nghi.t.tran8@gmail.com>
2020-03-11 08:43:48 -0700

    initial commit

---
+++ search.js
@@ -0,0 +1,10 @@
+/**
+ * #1
+ * Given a sorted list 3, 5, 6, 8, 11, 12, 14, 15, 17, 18 and using the recursive binary search algorithm, identify the sequence of numbers that each recursive call will search to try and find
+ *
+ * Middles: 11, 5, 6, 8
+ * 4 tries and found 8
+ *
+ * Middles: 11, 15, 17
+ * 3 tries and can't find 16
+ */
